6b5f8f76b6790b231d456c13630c6175d23f2a9b
Rich <rgnet1@yahoo.com>
2016-11-18 17:31:07 +0000

    disable CORS in non-prod env

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,8 +1,10 @@
 const hosts = require('./hosts');
-const whitelist = [hosts.devHost, hosts.prodHost];
+const whitelist = [hosts.prodHost];
 
 module.exports =  {
   origin: function(origin, callback){
+    if (process.env.NODE_ENV !== 'production') return callback(null);
+    
     var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
     callback(originIsWhitelisted ? null : 'Bad Request', originIsWhitelisted);
   }
